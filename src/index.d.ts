67752e193308423f81f66e6bfedd1d3265210507
lecepin <383810086@qq.com>
2021-08-11 22:39:11 +0800

    docs: d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+declare module "lp-logger" {
+  type Level = "error" | "warn" | "log";
+
+  interface IProp {
+    level?: Level;
+    name?: string;
+    search?: string;
+  }
+
+  export default class Logger {
+    constructor(prop?: IProp);
+    log(...args: any[]): void;
+    warn(...args: any[]): void;
+    error(...args: any[]): void;
+    debug(...args: any[]): void;
+    groupCollapsed(...args: any[]): void;
+    groupEnd(...args: any[]): void;
+  }
+}
