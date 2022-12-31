034df8c66d5035737e8c469c9d914129ee447a33
neila-a <neilaspace@outlook.com>
2022-12-31 12:33:22 +0800

    删除window

--- src/index.js
+++ src/index.js
@@ -1,63 +1,51 @@
 export default class Logger {
-  /**
-   * @param {String} level
-   *    error: 只显示 error
-   *    warn: 显示 warn/error
-   *    log: 显示所有
-   * @param {String} name
-   */
-  constructor({
-    level = "log",
-    name = "lp-logger",
-    search = "__lp_logger_level",
-  } = {}) {
-    const qs = new URLSearchParams(window.location.search);
-
-    if (Array.from(qs.keys()).includes(search)) {
-      this.level = qs.get(search);
-    } else {
-      this.level = level;
+    /**
+     * @param {String} level
+     *    error: 只显示 error
+     *    warn: 显示 warn/error
+     *    log: 显示所有
+     * @param {String} name
+     */
+    constructor({
+        level = "log",
+        name = "lp-logger",
+    } = {}) {
+        if (Array.from(qs.keys()).includes(search)) {
+            this.level = qs.get(search);
+        } else {
+            this.level = level;
+        }
+        this.name = name;
+        this.COLOR_MAP = {
+            error: "#f5222d", // 红
+            debug: "#7f8c8d", // 灰
+            log: "#52c41a", // 绿
+            warn: "#faad14", // 黄
+            groupEnd: null,
+        };
+        Object.keys(this.COLOR_MAP).map((key) => {
+            this[key] = (...args) => this._print(key, args);
+        });
     }
-
-    this.name = name;
-    this.COLOR_MAP = {
-      error: "#f5222d", // 红
-      debug: "#7f8c8d", // 灰
-      log: "#52c41a", // 绿
-      warn: "#faad14", // 黄
-      groupCollapsed: "#2f54eb", // 蓝
-      groupEnd: null,
+    _print(method, args) {
+        if (this.level === "error") {
+            if ("error" != method) {
+                return;
+            }
+        } else if (this.level === "warn") {
+            if (!["warn", "error"].includes(method)) {
+                return;
+            }
+        } else if (!this.level) {
+            return;
+        }
+        const logPrefix = [
+            "%c" + this.name,
+            `background:${this.COLOR_MAP[method]};border-radius: 0.5em;color: white;font-weight: bold;padding: 2px 0.5em`,
+        ];
+        console[method](...logPrefix, ...args);
+    }
+    info(...args) {
+        this.log(...args);
     };
-    Object.keys(this.COLOR_MAP).map((key) => {
-      this[key] = (...args) => this._print(key, args);
-    });
-  }
-
-  _print(method, args) {
-    if (this.level === "error") {
-      if ("error" != method) {
-        return;
-      }
-    } else if (this.level === "warn") {
-      if (!["warn", "error"].includes(method)) {
-        return;
-      }
-    } else if (!this.level) {
-      return;
-    }
-
-    if (method === "groupCollapsed") {
-      if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
-        console[method](...args);
-        return;
-      }
-    }
-
-    const logPrefix = [
-      "%c" + this.name,
-      `background:${this.COLOR_MAP[method]};border-radius: 0.5em;color: white;font-weight: bold;padding: 2px 0.5em`,
-    ];
-
-    console[method](...logPrefix, ...args);
-  }
 }
